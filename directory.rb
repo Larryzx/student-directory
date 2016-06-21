991c4539412c446103e1fd79a84983c12d63c7cb
Laurence Kite <laurent.kitecb@gmail.com>
2016-06-21 16:28:13 +0000

    Added a symbol to array so cohort can be printed

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 # let's put all students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter",:november ],
+  ["Darth Vader",:november],
+  ["Nurse Ratched",:november],
+  ["Michael Corleone",:november],
+  ["Alex DeLarge",:november],
+  ["The Wicked Witch of the West",:noevember],
+  ["Terminator",:noevember],
+  ["Freddy Krueger",:november],
+  ["The Joker",:november],
+  ["Joffrey Baratheon",:november],
+  ["Norman Bates",:november]
 ]
 def print_header 
   puts"The students of the Villians Academdy"
   puts "-------------"
 end 
-def print(names)
-  names.each do |name|
-    puts name     
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} #{student[1]} cohort"     
   end
 end
 def print_footer(names)
