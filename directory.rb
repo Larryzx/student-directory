dc225e6c1437aae56df67ca73d225f0d7b97b0f0
Laurence Kite <laurent.kitecb@gmail.com>
2016-06-28 18:01:54 +0000

    made the file use default students.csv if none loaded

--- directory.rb
+++ directory.rb
@@ -223,8 +223,12 @@ def load_students(filename = "students.csv")
   file.close
 end
 def try_load_students
-  filename = ARGV.first# first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
+  filename = ARGV.first # first argument from the command line
+  temp = ARGV[0]
+  if temp == nil #load default file
+    load_students
+  end  
+   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
      puts "Loaded #{@students.count} from #{filename}"
